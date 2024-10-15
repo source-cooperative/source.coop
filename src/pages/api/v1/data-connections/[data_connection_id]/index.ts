b58e8a80f24d5fd11d26c80da430fe17ae59d1d4
Kevin Booth <kevin@kb.gg>
2024-10-15 13:28:45 -0500

    Added debugging

--- src/pages/api/v1/data-connections/[data_connection_id]/index.ts
+++ src/pages/api/v1/data-connections/[data_connection_id]/index.ts
@@ -59,6 +59,7 @@ async function getDataConnectionHandler(
   console.log("Recevied: " + authorization);
   console.log("Equivalency: " + (authorization === process.env.SOURCE_KEY));
   if (authorization === process.env.SOURCE_KEY) {
+    console.log(dataConnection);
     res.status(StatusCodes.OK).json(dataConnection);
   } else {
     if (!isAuthorized(session, dataConnection, Actions.GetDataConnection)) {
