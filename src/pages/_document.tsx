784e06dd613ee2679d303c23f08cde377094afea
Jed Sundwall <jed@radiant.earth>
2024-10-23 22:19:11 -0400

    Update _document.tsx
    Updating hash for leaflet script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <script
           src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          integrity="sha256-db49d009c841f5ca34a888c96511ae936fd9f5533e90d8b2c4d57596f4e5641a"
         ></script>
       </Head>
       <body>
