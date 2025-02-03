82d107ef95044afeff7ba31a2a4cfc512879c710
Jed Sundwall <jed@radiant.earth>
2025-02-03 12:16:11 -0800

    New footer (#51)
    * Merge from DEV to Prod (#47)
    
    * fix: Fixed the issues with getting the local s3 objects when running application locally.
    
    Signed-off-by: Navdeep Gupta <navdeepg2021@gmail.com>
    
    * fix: fixed page loading issue.
    
    Signed-off-by: Navdeep Gupta <navdeepg2021@gmail.com>
    
    * Update RepositoryBrowser.tsx
    
    This should fix a bug that prints a duplicate repository ID when rendering an individual object's S3 URI.
    
    * fix type
    
    otherwise `npm run install-cli` does not work (see README)
    
    * Update AccessData.tsx (#23)
    
    * Update AccessData.tsx
    
    Fixing instruction to upload data to a repository.
    
    * Update AccessData.tsx
    
    * nit: reflect package version in lock file
    
    * ignore .env file to avoid leaking secrets
    
    + remove dump/ which is a duplicate line
    
    * install CLI package
    
    * add five missing environment variables to work locally
    
    * add install instructions for aws + fix details (#40)
    
    * we use squash merge -> not needed anymore (#41)
    
    * fix: s3 uri to cloud uri. (#45)
    
    Signed-off-by: Navdeep Gupta <navdeepg2021@gmail.com>
    
    * sorted data conections.
    
    * fixed the build
    
    * updated the version.
    
    ---------
    
    Signed-off-by: Navdeep Gupta <navdeepg2021@gmail.com>
    Co-authored-by: Navdeep Gupta <navdeepg2021@gmail.com>
    Co-authored-by: navdeepg2021 <89108933+navdeepg2021@users.noreply.github.com>
    Co-authored-by: Jed Sundwall <jed@radiant.earth>
    Co-authored-by: Sylvain Lesage <severo@rednegra.net>
    
    * Added Select region as default. (#49)
    
    * new footer layout
    
    ---------
    
    Signed-off-by: Navdeep Gupta <navdeepg2021@gmail.com>
    Co-authored-by: venkat-ecov-dev <ecovdev@ecovoice.ca>
    Co-authored-by: Navdeep Gupta <navdeepg2021@gmail.com>
    Co-authored-by: navdeepg2021 <89108933+navdeepg2021@users.noreply.github.com>
    Co-authored-by: Sylvain Lesage <severo@rednegra.net>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,58 +1,64 @@
 /** @jsxImportSource theme-ui */
 
-import { Text, Grid } from "theme-ui";
+import { Container, Text, Grid, Box } from "theme-ui";
 import SourceLink from "@/components/SourceLink";
 
 const footerLinks = [
   { url: "/", display: "Home", external: false },
   { url: "/repositories", display: "Repositories", external: false },
   { url: "https://docs.source.coop", display: "Docs", external: true },
-  { url: "mailto:hello@source.coop", display: "Contact", external: false },
-  {
-    url: "https://join.slack.com/t/sourcecoop/shared_invite/zt-212sakf1j-fONCD4lZ_v2HP2PDpTr2dw",
-    display: "Slack",
-    external: true,
-  },
-  {
-    url: "https://forms.gle/fjMpYrwpVZEaBExW6",
-    display: "Publish Data",
-    external: true,
-  },
-  { url: "https://radiant.earth", display: "Radiant Earth", external: true },
 ];
 
 export function Footer({}) {
   return (
-    <Grid
-      sx={{
-        backgroundColor: "footerBackground",
-        gridTemplateColumns: [
-          "auto auto",
-          "auto auto auto",
-          "auto auto auto",
-          "auto auto auto auto",
-        ],
-        rowGap: 1,
-        mt: 2,
-        py: 3,
-        px: [2, 2, 3, 4],
-      }}
-    >
-      {footerLinks.map(({ url, display, external }, i) => {
-        if (url) {
-          return (
-            <SourceLink
-              key={"footer-link-" + i}
-              href={url}
-              variant="footer"
-              sx={{ textAlign: "center" }}
-            >
-              <Text>{display}</Text>
-            </SourceLink>
-          );
-        }
-        return <Text variant="footer">{display}</Text>;
-      })}
-    </Grid>
+    <Container
+        sx={{
+          width: ["100%", "100%", "100%", "70%"],
+          margin: "0 auto",
+          px: 3,
+        }}
+      >
+        <Grid
+          sx={{
+            gridTemplateColumns: ["auto"],
+            rowGap: 1,
+            justifyContent: "flex-start",
+            textAlign: "left",
+          }}
+        >
+          {footerLinks.map(({ url, display }, i) => (
+            url ? (
+              <SourceLink
+                key={"footer-link-" + i}
+                href={url}
+                variant="footer"
+              >
+                <Text>{display}</Text>
+              </SourceLink>
+            ) : (
+              <Text key={"footer-text-" + i} variant="footer" sx={{ textAlign: "left" }}>
+                {display}
+              </Text>
+            )
+          ))}
+          <Text variant="footer">
+            <Box
+                as="hr"
+                sx={{
+                  border: "none",      // Removes default styles
+                  borderBottom: "1px solid",
+                  borderColor: "muted", // Uses the Theme UI muted color (adjust as needed)
+                  my: 3,               // Adds spacing above and below the line
+                  width: "100%",       // Makes sure it spans the full width
+                }}
+              />
+              Source Cooperative is a{" "}
+              <SourceLink href="https://radiant.earth" variant="footer">
+                Radiant Earth
+              </SourceLink>
+              {" "}initiative.
+          </Text>
+        </Grid>
+      </Container>
   );
 }
