d0e04dd6cb1f6824ec7b3f9c67c3adc1a4364627
Anthony Lukach <anthonylukach@gmail.com>
2025-04-04 12:03:06 -0700

    fix: use suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,12 @@ import { metadata } from './metadata';
 import { Navigation, Footer } from '@/components/layout';
 import { Box } from '@radix-ui/themes';
 import { SessionProvider } from '@/components/providers/SessionProvider';
+import { Suspense } from "react";
 
 const ibmPlexSans = IBM_Plex_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
 });
 
 export { metadata };
@@ -30,8 +31,10 @@ export default function RootLayout({
           storageKey="source-theme"
         >
           <SessionProvider>
-            <Box style={{ minHeight: '100vh' }}>
-              <Navigation />
+            <Box style={{ minHeight: "100vh" }}>
+              <Suspense>
+                <Navigation />
+              </Suspense>
               <Box asChild my="6">
                 <main>{children}</main>
               </Box>
