4cae765419afa1595a6b90f48c09192fa8ea6246
Anthony Lukach <anthonylukach@gmail.com>
2025-04-23 16:19:20 -0700

    Test doing logout server-side

--- src/app/auth/logout/route.tsx
+++ src/app/auth/logout/route.tsx
@@ -1,10 +1,28 @@
 import { CONFIG } from "@/lib/config";
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
-  const returnTo = new URL(request.url);
-  return NextResponse.redirect(
-    `${CONFIG.auth.routes.logout}?return_to=${returnTo.origin}`,
-    302
-  );
+export async function GET(request: NextRequest) {
+  const returnTo = new URL(request.url).origin;
+  const url = new URL(CONFIG.auth.routes.logout);
+  url.searchParams.set("return_to", returnTo);
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: request.headers.get("cookie") || "",
+    },
+  });
+  if (!response.ok) {
+    console.error("Logout failed", response.status, response.statusText);
+    return NextResponse.json(
+      {
+        error: "Logout failed",
+        status: response.status,
+        text: await response.text(),
+      },
+      { status: response.status }
+    );
+  }
+  const data = await response.json();
+  return NextResponse.redirect(data.logout_url, 302);
 }
