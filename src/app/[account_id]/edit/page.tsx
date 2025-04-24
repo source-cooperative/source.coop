8dfe7c704fedf45e9776c460630e21c8f621886d
Anthony Lukach <anthonylukach@gmail.com>
2025-04-23 21:23:29 -0700

    Only allow users to edit their own account

--- src/app/[account_id]/edit/page.tsx
+++ src/app/[account_id]/edit/page.tsx
@@ -1,17 +1,34 @@
 import { notFound } from "next/navigation";
+import { getServerSession } from "@ory/nextjs/app";
+import { Container, Heading, Text } from "@radix-ui/themes";
 import { fetchAccount } from "@/lib/db";
+import { getAccountId } from "@/lib/ory";
 import { EditProfileForm } from "./EditProfileForm";
 
-export default async function EditProfilePage({
-  params,
-}: {
-  params: Promise<{ account_id: string }>;
-}) {
+type Params = Promise<{ account_id: string }>;
+
+export default async function EditProfilePage({ params }: { params: Params }) {
   const { account_id } = await params;
   const account = await fetchAccount(account_id);
+  const session = await getServerSession();
 
   if (!account) {
     notFound();
+  }
+
+  const userAccount = getAccountId(session);
+  if (userAccount !== account_id) {
+    return (
+      <Container size="2" py="6">
+        <Heading size="6" mb="4">
+          Access Denied
+        </Heading>
+
+        <Text as="p" size="3" color="gray" className="mb-4">
+          You do not have permission to edit this profile.
+        </Text>
+      </Container>
+    );
   }
 
   return (
