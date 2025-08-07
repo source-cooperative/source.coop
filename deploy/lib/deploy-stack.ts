5f105dcb63e69b0e3b14edd3b4160599727dfb50
Anthony Lukach <anthonylukach@gmail.com>
2025-08-07 10:48:28 -0700

    feat: implement Vercel integration and enhance database resource management

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,18 +1,38 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { DatabaseConstruct } from "./database-construct";
+import { VercelConstruct } from "./vercel-construct";
 
 export class ApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
     // Create database resources
-    const database = new DatabaseConstruct(this, "Database", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // For development - change to RETAIN for production
+    const database = new DatabaseConstruct(this, "database", {
+      removalPolicy:
+        props.stage === "prod"
+          ? cdk.RemovalPolicy.RETAIN
+          : cdk.RemovalPolicy.DESTROY,
     });
 
-    // You can now access the tables via:
-    // database.accountsTable
-    // database.productsTable
+    // Create Vercel role with OIDC trust relationship
+    const vercel = new VercelConstruct(this, "vercel", {
+      projectName: "radiantearth",
+      stage: props.stage,
+    });
+
+    for (const table of [
+      database.accountsTable,
+      database.productsTable,
+      database.dataConnectionsTable,
+      database.apiKeysTable,
+      database.membershipsTable,
+    ]) {
+      table.grantReadWriteData(vercel.vercelRole);
+    }
   }
+}
+
+interface ApiStackProps extends cdk.StackProps {
+  readonly stage: string;
 }
