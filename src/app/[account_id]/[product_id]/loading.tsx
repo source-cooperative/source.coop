8f5478054893ed17e957b17c91b84b9aa085212b
Anthony Lukach <anthonylukach@gmail.com>
2025-08-18 22:59:51 -0700

    make loading skeleton more like reality

--- src/app/[account_id]/[product_id]/loading.tsx
+++ src/app/[account_id]/[product_id]/loading.tsx
@@ -1,55 +1,42 @@
-import { Container, Box, Flex, Card, Text } from '@radix-ui/themes';
-import { Skeleton } from '@/components/core';
+import { Box, Flex, Card } from "@radix-ui/themes";
+import { SectionHeader, Skeleton } from "@/components/core";
 
 export default function ProductPageLoading() {
   return (
-    <Container>
-      {/* Product Header Skeleton */}
-      <Box mb="6">
-        <Flex direction="column" gap="3">
-          <Skeleton height="32px" width="60%" />
-          <Skeleton height="20px" width="40%" />
-          <Flex gap="2" mt="2">
-            <Skeleton height="24px" width="80px" />
-            <Skeleton height="24px" width="100px" />
+    <Card>
+      <Box pt="0">
+        {/* Breadcrumb Skeleton */}
+        <SectionHeader title="Product Contents">
+          <Flex gap="2" mb="4" align="center">
+            <Skeleton height="16px" width="60px" />
+            <Skeleton height="16px" width="16px" />
+            <Skeleton height="16px" width="80px" />
           </Flex>
-        </Flex>
-      </Box>
+        </SectionHeader>
 
-      {/* Object Browser Skeleton */}
-      <Box mt="4">
-        <Card>
-          <Box p="4">
-            {/* Breadcrumb Skeleton */}
-            <Flex gap="2" mb="4" align="center">
-              <Skeleton height="16px" width="60px" />
+        {/* Directory List Skeleton */}
+        <Box>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Flex
+              key={index}
+              align="center"
+              gap="3"
+              px="1"
+              py="2"
+              style={{
+                borderTop: index < 7 ? "1px solid var(--gray-5)" : "none",
+                borderBottom: index < 7 ? "1px solid var(--gray-5)" : "none",
+              }}
+            >
               <Skeleton height="16px" width="16px" />
-              <Skeleton height="16px" width="80px" />
+              <Skeleton height="16px" width={`${60 + Math.random() * 40}%`} />
+              <Box ml="auto">
+                <Skeleton height="16px" width="60px" />
+              </Box>
             </Flex>
-
-            {/* Directory List Skeleton */}
-            <Box>
-              {Array.from({ length: 8 }).map((_, index) => (
-                <Flex
-                  key={index}
-                  align="center"
-                  gap="3"
-                  p="2"
-                  style={{
-                    borderBottom: index < 7 ? '1px solid var(--gray-5)' : 'none',
-                  }}
-                >
-                  <Skeleton height="16px" width="16px" />
-                  <Skeleton height="16px" width={`${60 + Math.random() * 40}%`} />
-                  <Box ml="auto">
-                    <Skeleton height="16px" width="60px" />
-                  </Box>
-                </Flex>
-              ))}
-            </Box>
-          </Box>
-        </Card>
+          ))}
+        </Box>
       </Box>
-    </Container>
+    </Card>
   );
 }
