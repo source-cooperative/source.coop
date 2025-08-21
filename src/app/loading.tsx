d5b58b32de1b8c74b1607c63da37e5bf45eed823
Anthony Lukach <anthonylukach@gmail.com>
2025-08-21 06:01:55 -0700

    Remove descriptions

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,10 +4,7 @@ import { PageContainer, PageHeader } from "@/components/layout";
 export default function HomeLoading() {
   return (
     <PageContainer>
-      <PageHeader
-        title="Featured Products"
-        description="Discover the latest and most popular data products from the Source.coop community."
-      />
+      <PageHeader title="Featured Products" />
       <ProductsSkeleton />
     </PageContainer>
   );
