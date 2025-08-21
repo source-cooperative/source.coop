562ffb05cdfd94864d489fc02dfb09b91c3fa801
Anthony Lukach <anthonylukach@gmail.com>
2025-08-21 14:22:38 -0700

    Rework account fetch & email management into helper function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,19 @@
-import "@radix-ui/themes/styles.css";
-import "@/styles/globals.css";
-import { ThemeProvider } from "@/styles/theme";
-import { SessionProvider } from "@ory/elements-react/client";
-import { getServerSession } from "@ory/nextjs/app";
-import NextTopLoader from "nextjs-toploader";
 import { IBM_Plex_Sans } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 import { Box } from "@radix-ui/themes";
-import { Navigation, Footer /*, Banner*/ } from "@/components/layout";
+import { SessionProvider } from "@ory/elements-react/client";
 import { Suspense } from "react";
+import NextTopLoader from "nextjs-toploader";
+import { getAccountFromSession } from "@/lib/actions/account";
+import { Navigation } from "@/components/layout";
+import { Footer } from "@/components/layout";
 import { metadata } from "./metadata";
 import { CONFIG } from "@/lib/config";
-import { getOryId } from "@/lib/ory";
-import { accountsTable } from "@/lib/clients/database";
-import { Account } from "@/types/account";
-import { redirect, RedirectType } from "next/navigation";
+
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex",
+  variable: "--font-ibm-plex-sans",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export { metadata };
@@ -27,36 +23,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
-  let account: Account | null = null;
-  if (session) {
-    const oryId = getOryId(session);
-    if (oryId) {
-      account = await accountsTable.fetchByOryId(oryId);
-      // If we have a session but no account, that means a user is authenticated but we need
-      // to redirect to the email verification page so that a user can setup their account.
-      if (!account) {
-        redirect("/onboarding", RedirectType.replace);
-      }
-
-      // If we have an account but no email, we add the email from the session.
-      if (account.emails?.length === 0 && session.identity?.traits.email) {
-        account.emails = [
-          {
-            address: session.identity.traits.email,
-            verified: false,
-            is_primary: true,
-            added_at: new Date().toISOString(),
-          },
-        ];
-        try {
-          await accountsTable.update(account);
-        } catch (error) {
-          console.error("Failed to add email to account", error);
-        }
-      }
-    }
-  }
+  const account = await getAccountFromSession();
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -68,10 +35,7 @@ export default async function RootLayout({
           storageKey="source-theme"
         >
           <NextTopLoader />
-          <SessionProvider
-            session={session}
-            baseUrl={CONFIG.auth.api.frontendUrl}
-          >
+          <SessionProvider baseUrl={CONFIG.auth.api.frontendUrl}>
             <Box style={{ minHeight: "100vh" }}>
               {/* <Banner /> */}
               <Suspense>
