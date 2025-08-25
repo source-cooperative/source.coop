fee8d657db0d38f407afb7eedb35d030419443b9
Anthony Lukach <anthonylukach@gmail.com>
2025-08-25 09:30:42 -0700

    Rework layout and auth login UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,12 @@ import "@radix-ui/themes/styles.css";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/styles/theme";
 import { SessionProvider } from "@ory/elements-react/client";
-import { getServerSession } from "@ory/nextjs/app";
 import NextTopLoader from "nextjs-toploader";
 import { IBM_Plex_Sans } from "next/font/google";
 import { Box } from "@radix-ui/themes";
 import { Navigation, Footer /*, Banner*/ } from "@/components/layout";
 import { Suspense } from "react";
 import { metadata } from "./metadata";
-import { CONFIG } from "@/lib";
-import { getOryId } from "@/lib/ory";
-import { accountsTable } from "@/lib/clients/database";
-import { Account } from "@/types/account";
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -21,21 +16,7 @@ const ibmPlexSans = IBM_Plex_Sans({
 
 export { metadata };
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerSession();
-  let account: Account | null = null;
-
-  if (session) {
-    const oryId = getOryId(session);
-    if (oryId) {
-      account = await accountsTable.fetchByOryId(oryId);
-    }
-  }
-
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={ibmPlexSans.variable}>
@@ -46,14 +27,11 @@ export default async function RootLayout({
           storageKey="source-theme"
         >
           <NextTopLoader />
-          <SessionProvider
-            session={session}
-            baseUrl={CONFIG.auth.api.frontendUrl}
-          >
+          <SessionProvider>
             <Box style={{ minHeight: "100vh" }}>
               {/* <Banner /> */}
               <Suspense>
-                <Navigation account={account} />
+                <Navigation />
               </Suspense>
               <Box asChild my="6">
                 <main>{children}</main>
