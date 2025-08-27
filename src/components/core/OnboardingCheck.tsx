fce05f43a02e382a3d61799484a84bda5e2508d2
Anthony Lukach <anthonylukach@gmail.com>
2025-08-26 22:25:22 -0700

    Log pathname and account

--- src/components/core/OnboardingCheck.tsx
+++ src/components/core/OnboardingCheck.tsx
@@ -14,6 +14,7 @@ export function OnboardingCheck({ account }: { account?: Account }) {
   const pathname = usePathname();
 
   useEffect(() => {
+    console.log({ pathname, account });
     if (pathname !== "/onboarding" && !account) {
       redirect("/onboarding");
     }
