79e9a3b3fa5f86060027ad56c241b8d277b6b7e6
Anthony Lukach <anthonylukach@gmail.com>
2025-09-03 14:47:34 -0700

    Support external auth to retain production passkeys

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,6 +3,8 @@ import { awsCredentialsProvider } from "@vercel/functions/oidc";
 
 const region = process.env.AWS_REGION || "us-east-1";
 
+const frontendUrl = process.env.NEXT_PUBLIC_ORY_UI_URL || "";
+
 export const CONFIG = {
   storage: {
     type: "S3",
@@ -27,15 +29,16 @@ export const CONFIG = {
   auth: {
     api: {
       backendUrl: process.env.NEXT_PUBLIC_ORY_SDK_URL,
+      frontendUrl,
     },
     accessToken: process.env.ORY_PROJECT_API_KEY || "",
 
     routes: {
       // We use middleware to serve auth pages.
       // https://www.ory.sh/docs/reference/api#tag/frontend/operation/createBrowserLoginFlow
-      login: `/self-service/login/browser`,
+      login: `${frontendUrl}/self-service/login/browser`,
       // https://www.ory.sh/docs/reference/api#tag/frontend/operation/createBrowserLogoutFlow
-      logout: `/self-service/logout/browser`,
+      logout: `${frontendUrl}/self-service/logout/browser`,
     },
   },
   google: {
