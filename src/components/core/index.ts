fc3bcd9fd2b974c53bb49596d0de979ffa1345cf
Anthony Lukach <anthonylukach@gmail.com>
2025-09-10 20:09:48 -0700

    Buildout loading views for forms

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -2,6 +2,7 @@ export { LinkCard } from "./LinkCard";
 export { MonoText } from "./MonoText";
 export { SectionHeader } from "./SectionHeader";
 export { Skeleton } from "./Skeleton";
+export { FormSkeleton } from "./FormSkeleton";
 export { DynamicForm } from "./DynamicForm";
 export type { FormField } from "./DynamicForm";
 export { SmallColumnContainer } from "./SmallColumnContainer";
