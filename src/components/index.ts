988fd874145cd88e03eefded730a26fe0a30b5f3
Anthony Lukach <anthonylukach@gmail.com>
2025-09-21 15:17:47 -0700

    Add chevron icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,15 @@
 // Core UI components
 export * from "./core";
 export * from "./display";
+export * from "./icons";
 export * from "./layout";
 
 // Feature components
 // export * from './features/markdown';  // Avoid problematic import of ./features/markdown/codeConfig
 export * from "./features/accounts";
-export * from './features/metadata';
-export * from './features/profiles';
-export * from './features/products';
+export * from "./features/metadata";
+export * from "./features/profiles";
+export * from "./features/products";
 export * from "./features/profiles";
 export * from "./features/products";
 export * from "./features/profiles";
