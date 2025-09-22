9d5c2040272f199c99e936f03f485e11360f42f8
Sylvain Lesage <severo@rednegra.net>
2025-09-22 12:05:10 +0200

    pass theme parameter to parquet-table iframe

--- src/components/features/products/object-browser/ObjectPreview.tsx
+++ src/components/features/products/object-browser/ObjectPreview.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { Box } from "@radix-ui/themes";
+import { useTheme } from 'next-themes';
 
 interface ObjectPreviewProps {
   sourceUrl: string;
 }
 
 const getIframeSrc = (sourceUrl: string) => {
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === 'dark' ? 'dark' : 'light';
+
   switch (sourceUrl.split(".").pop()) {
     case "pmtiles":
+      // TODO(SL): add support for theme param in pmtiles.io?
       return `https://pmtiles.io/#url=${sourceUrl}&iframe=true`;
     case "parquet":
-      return `https://source-cooperative.github.io/parquet-table/?iframe&url=${sourceUrl}`;
+      return `https://source-cooperative.github.io/parquet-table/?url=${sourceUrl}&iframe=true&theme=${theme}`;
     default:
       return null;
   }
