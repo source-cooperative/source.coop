7610b20ae287ee3a2cd15c3584370d4f77a4e073
Anthony Lukach <anthonylukach@gmail.com>
2025-09-30 16:03:41 -0700

    Fix imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,23 +5,12 @@ export * from "./icons";
 export * from "./layout";
 
 // Feature components
-// export * from './features/markdown';  // Avoid problematic import of ./features/markdown/codeConfig
-export * from './features/metadata';
-export * from './features/profiles';
-export * from './features/products';
-export * from "./features/profiles";
-export * from "./features/products";
-export * from "./features/profiles";
-export * from "./features/products";
-export * from "./features/profiles";
-export * from "./features/onboarding";
+export * from "./features/accounts";
 export * from "./features/keyboard";
-
-// Metadata components
+// export * from './features/markdown';  // Avoid problematic import of ./features/markdown/codeConfig
 export * from "./features/metadata";
-
-// Profile components
-export * from "./features/profiles";
-
-// Repository components
+export * from "./features/onboarding";
+export * from "./features/organizations";
 export * from "./features/products";
+export * from "./features/profiles";
+export * from "./features/settings";
