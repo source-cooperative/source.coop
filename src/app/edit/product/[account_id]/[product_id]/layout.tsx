557831e797961002221c70f922e621509bb72d68
Anthony Lukach <anthonylukach@gmail.com>
2025-10-03 08:25:55 -0700

    Add "view product" link

--- src/app/edit/product/[account_id]/[product_id]/layout.tsx
+++ src/app/edit/product/[account_id]/[product_id]/layout.tsx
@@ -11,17 +11,17 @@ import {
   GlobeIcon,
   ArchiveIcon,
   Pencil1Icon,
+  ExternalLinkIcon,
 } from "@radix-ui/react-icons";
 import { AvatarLinkCompact, MonoText } from "@/components";
 import {
-  accountUrl,
   productUrl,
   editProductDetailsUrl,
   editProductAccessUrl,
   editProductVisibilityUrl,
   editProductArchiveUrl,
 } from "@/lib/urls";
-import { Box, Flex, Text, Button } from "@radix-ui/themes";
+import { Box, Flex, Text, Button, Link as RadixLink } from "@radix-ui/themes";
 import Link from "next/link";
 
 interface ProductLayoutProps {
@@ -106,16 +106,27 @@ export default async function ProductLayout({
 function ProductHeader({ product }: { product: Product }) {
   return (
     <Box mb="5" pb="4" style={{ borderBottom: "1px solid var(--gray-6)" }}>
-      <Flex align="center" gap="3">
-        <AvatarLinkCompact account={product.account!} />
-        <Text size="2" color="gray" style={{ userSelect: "none" }}>
-          &gt;
-        </Text>
-        <Button variant="ghost" asChild>
-          <Link href={productUrl(product.account_id, product.product_id)}>
-            <MonoText>{product.product_id}</MonoText>
-          </Link>
-        </Button>
+      <Flex justify="between" align="center">
+        {/* Left side - Product account and id */}
+        <Flex align="center" gap="3">
+          <AvatarLinkCompact account={product.account!} />
+          <Text size="2" color="gray" style={{ userSelect: "none" }}>
+            &gt;
+          </Text>
+          <Button variant="ghost" asChild>
+            <Link href={productUrl(product.account_id, product.product_id)}>
+              <MonoText>{product.product_id}</MonoText>
+            </Link>
+          </Button>
+        </Flex>
+
+        {/* Right side - Link to product */}
+        <RadixLink href={productUrl(product.account_id, product.product_id)}>
+          <Flex align="center" gap="2">
+            <Text size="1">View Product</Text>
+            <ExternalLinkIcon width="14" height="14" />
+          </Flex>
+        </RadixLink>
       </Flex>
     </Box>
   );
