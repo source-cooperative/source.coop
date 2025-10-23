b7737712cec2549dcce514d67f187a551f7d59ef
Anthony Lukach <anthonylukach@gmail.com>
2025-10-23 15:41:06 -0700

    Fix for loop

--- deploy/lib/vercel-construct.ts
+++ deploy/lib/vercel-construct.ts
@@ -50,9 +50,7 @@ export class VercelConstruct extends Construct {
 
     // Grant permissions for multipart uploads and reads
     // (session policies will further restrict access to specific prefixes)
-    console.log({ writableBuckets: props.writableBuckets });
-    for (const bucketName in props.writableBuckets) {
-      console.log({ bucketName });
+    for (const bucketName of props.writableBuckets) {
       const bucket = s3.Bucket.fromBucketName(
         this,
         `bucket-${bucketName}`,
