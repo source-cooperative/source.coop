ff77848844af3e0fa4d297e6e2d0d991d42ea7ec
Anthony Lukach <anthonylukach@gmail.com>
2025-10-23 14:52:27 -0700

    Add role for s3 uploads

--- deploy/lib/vercel-construct.ts
+++ deploy/lib/vercel-construct.ts
@@ -1,15 +1,18 @@
 import * as cdk from "aws-cdk-lib";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export interface VercelConstructProps {
   readonly projectName: string;
   readonly stage: string;
   readonly vercelEnvironment: string[];
+  readonly s3Bucket: s3.IBucket;
 }
 
 export class VercelConstruct extends Construct {
   public readonly vercelRole: iam.Role;
+  public readonly s3AccessRole: iam.Role;
 
   constructor(scope: Construct, id: string, props: VercelConstructProps) {
     super(scope, id);
@@ -35,10 +38,40 @@ export class VercelConstruct extends Construct {
       ),
     });
 
+    this.s3AccessRole = new iam.Role(this, "s3-upload-role", {
+      roleName: `SourceFrontend-S3UploadAccess-${props.stage}`,
+      description:
+        "Role used by Vercel to generate temporary credentials for S3 uploads",
+      assumedBy: this.vercelRole,
+    });
+
+    // Grant Vercel role permission to assume this role
+    this.s3AccessRole.grantAssumeRole(this.vercelRole);
+
+    // Grant permissions for multipart uploads and reads
+    // (session policies will further restrict access to specific prefixes)
+    props.s3Bucket.grantReadWrite(this.s3AccessRole);
+
+    this.s3AccessRole.addToPolicy(
+      new iam.PolicyStatement({
+        sid: "AllowListBucket",
+        effect: iam.Effect.ALLOW,
+        actions: ["s3:ListBucket"],
+        resources: [props.s3Bucket.bucketArn],
+      })
+    );
+
     // Output the role ARN for reference
     new cdk.CfnOutput(this, "VercelRoleArn", {
       value: this.vercelRole.roleArn,
       description: "ARN of the Vercel IAM role",
+    });
+
+    new cdk.CfnOutput(this, "S3UploadAccessRoleArn", {
+      value: this.s3AccessRole.roleArn,
+      description:
+        "ARN of the S3 upload access role for STS temporary credentials",
+      exportName: `SourceFrontend-S3UploadAccessRoleArn-${props.stage}`,
     });
   }
 }
