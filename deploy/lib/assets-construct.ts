7f85d3918a6aaf9ee8502f27e55db332f8ffbe07
Anthony Lukach <anthonylukach@gmail.com>
2025-12-18 11:32:27 -0800

    feat: add organization profile image (#193)
    ## What I'm changing
    
    This PR adds the ability to upload profiles images for either Individual
    or Organization accounts.
    
    ### demo: Organizations can upload a profile image
    
    
    https://github.com/user-attachments/assets/2725334e-bba1-4842-b991-b7abe73833a2
    
    <img width="1200" height="630" alt="image"
    src="https://github.com/user-attachments/assets/150d929c-62d9-406e-98c1-06d18b740869"
    />
    
    ### demo: user accounts can override their gravatar profile image
    
    
    https://github.com/user-attachments/assets/c0dd442c-d18e-4012-9fd2-65fbf11e4d60
    
    <!--
      High-level summary of what is achieved by these changes.
    
      Optional: reference related issues.
    -->
    
    ## How I did it
    
    This PR updates the CDK tooling to:
    
    * create a private assets bucket
    * create a cloudfront distribution that serves the assets from the
    assets bucket
    * permit the IAM Role used by Vercel to be able to read/write/delete
    files from the assets bucket
    
    Uploads occur via a two step process:
    
    1. We make a POST request to the frontend to create a signed-url that
    allows writing an asset to a specific key
    1. We make a PUT request to upload the asset to a new assets bucket
    
    Once an profile image is uploaded, its URL is stored on the account
    record.
    
    The uploaded files must abide by the following restrictions:
    
    
    https://github.com/source-cooperative/source.coop/blob/469902d21980cc49cf608a52fcdc3c85f3b594e7/src/components/features/profiles/ProfileImageUpload.tsx#L19-L20
    
    The account profile images are able to be edited within a new menu in
    the account settings. Images can either be uploaded (including replacing
    existing images) or deleted.
    
    <!--
    
      Lower-level details of the steps taken to achieve goal.
    
      Include:
        - considerations made when deciding how to implement feature
        - any changes to API that could impact the Data Proxy
    -->
    
    ## How you can test it
    
    <!--
      Instructions on how a reviewer can verify these changes.
    
      Consider including screenshots or video demonstrating feature.
    -->
    
    Test via UI at
    https://source-cooperative-git-feat-organization-pr-8f2942-radiantearth.vercel.app

---
+++ deploy/lib/assets-construct.ts
@@ -0,0 +1,111 @@
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { Construct } from "constructs";
+
+export interface AssetsConstructProps {
+  readonly bucketName: string;
+  readonly allowedOrigins: string[];
+  readonly removalPolicy?: cdk.RemovalPolicy;
+}
+
+/**
+ * Creates an S3 bucket for serving static assets like profile images, logos, etc.
+ *
+ * Features:
+ * - S3 bucket with versioning and server-side encryption
+ * - CORS configuration for web uploads
+ * - CloudFront distribution for CDN delivery
+ * - Origin Access Control for secure S3 access
+ */
+export class AssetsConstruct extends Construct {
+  public readonly bucket: s3.Bucket;
+  public readonly distribution: cloudfront.Distribution;
+  public readonly distributionDomainName: string;
+
+  constructor(scope: Construct, id: string, props: AssetsConstructProps) {
+    super(scope, id);
+
+    const { removalPolicy = cdk.RemovalPolicy.RETAIN } = props;
+
+    // Create S3 bucket for assets
+    this.bucket = new s3.Bucket(this, "assets-bucket", {
+      bucketName: props.bucketName,
+      removalPolicy,
+      autoDeleteObjects: removalPolicy === cdk.RemovalPolicy.DESTROY,
+      versioned: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      publicReadAccess: false, // CloudFront will access via OAC
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.PUT,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.HEAD,
+          ],
+          allowedOrigins: props.allowedOrigins,
+          allowedHeaders: ["*"],
+          exposedHeaders: ["ETag"],
+          maxAge: 3000,
+        },
+      ],
+    });
+
+    // Create CloudFront distribution
+    this.distribution = new cloudfront.Distribution(
+      this,
+      "assets-distribution",
+      {
+        defaultBehavior: {
+          origin: origins.S3BucketOrigin.withOriginAccessIdentity(this.bucket),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+          cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+          compress: true,
+        },
+        comment: `Assets CDN for ${props.bucketName}`,
+        enabled: true,
+        httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_100, // Use only North America and Europe edge locations
+      }
+    );
+
+    this.distributionDomainName = this.distribution.distributionDomainName;
+
+    // Outputs
+    new cdk.CfnOutput(this, "AssetsBucketName", {
+      value: this.bucket.bucketName,
+      description: "Name of the assets S3 bucket",
+      exportName: `sc-assets-bucket-name`,
+    });
+
+    new cdk.CfnOutput(this, "AssetsBucketArn", {
+      value: this.bucket.bucketArn,
+      description: "ARN of the assets S3 bucket",
+      exportName: `sc-assets-bucket-arn`,
+    });
+
+    new cdk.CfnOutput(this, "AssetsBucketDomainName", {
+      value: this.bucket.bucketDomainName,
+      description: "Domain name of the assets S3 bucket",
+      exportName: `sc-assets-bucket-domain`,
+    });
+
+    new cdk.CfnOutput(this, "AssetsDistributionId", {
+      value: this.distribution.distributionId,
+      description: "CloudFront distribution ID for assets",
+      exportName: `sc-assets-distribution-id`,
+    });
+
+    new cdk.CfnOutput(this, "AssetsDistributionDomainName", {
+      value: this.distributionDomainName,
+      description: "CloudFront distribution domain name for assets",
+      exportName: `sc-assets-distribution-domain`,
+    });
+  }
+}
