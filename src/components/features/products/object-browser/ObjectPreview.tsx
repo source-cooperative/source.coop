775fc4cdf96126606fa9f62d26fd0394a8972fce
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2026-02-12 09:20:14 -0800

    feat: Add full screen support for ObjectPreview iframes (#224)
    ## What I'm changing
    
    This change enables the ObjectPreview `iframe` element to launch full
    screen mode for viewers that request it, such as the image and 3D model
    viewers from https://github.com/source-cooperative/source.coop/pull/210.
    
    ## How I did it
    
    I've added the permissions policy `allow="fullscreen"` to the `iframe`
    in ObjectPreview. This enables any current or future viewer to launch
    full screen mode.
    
    An alternative approach would be to make `allow` a custom per-format
    attribute like `style`, but this seems simpler.
    
    ## How you can test it
    
    On a preview branch where this feature is active, navigate to the
    following pages and click the "Toggle full screen" button within the
    preview `iframe`:
    
    *
    https://source.coop/harvard-lil/smithsonian-open-access/media/nmnh/NMNH-00000406-000001.jpg
    *
    https://source.coop/harvard-lil/smithsonian-open-access/3d/009463d3-6f58-4f5b-8e60-915805a876ee/USNM_91201-150k-1024-low.glb
    
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>

--- src/components/features/products/object-browser/ObjectPreview.tsx
+++ src/components/features/products/object-browser/ObjectPreview.tsx
@@ -64,7 +64,7 @@ export function ObjectPreview({ sourceUrl: cloudUri }: ObjectPreviewProps) {
     const { src, style } = iframeProps;
     return (
       <Box mt="4" pt="4" style={{ borderTop: "1px solid var(--gray-6)" }}>
-        <iframe width="100%" height="600px" style={style} src={src}>
+        <iframe width="100%" height="600px" allow="fullscreen" style={style} src={src}>
           Your browser does not support iframes.
         </iframe>
       </Box>
