1aa1e034dae317f5f348cb0279443b54e89c8c6b
Sylvain Lesage <severo@rednegra.net>
2026-02-13 18:21:27 +0100

    handle 'file.' with an ending dot

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,7 +1,11 @@
 export const getExtension = (object_path: string): string | undefined => {
   const parts = object_path.split('/').pop()?.split(".");
   if (parts && parts.length > 1) {
-    return parts.pop()?.toLowerCase();
+    const extension = parts.pop()
+    if (extension) {
+      // extension is a non-empty string, return it in lowercase
+      return extension.toLowerCase();
+    }
   }
   return undefined;
 };
