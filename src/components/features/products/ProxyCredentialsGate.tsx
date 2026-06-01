8bd17bb49e575a1c9bc6f1b3003a54ebce1fb2dd
Anthony Lukach <anthonylukach@gmail.com>
2026-06-01 09:11:56 -0700

    chore: minor ui tweak

--- src/components/features/products/ProxyCredentialsGate.tsx
+++ src/components/features/products/ProxyCredentialsGate.tsx
@@ -110,7 +110,7 @@ export function ProxyCredentialsGate() {
   }
 
   return (
-    <Flex align="center" gap="2" py="4">
+    <Flex align="center" gap="2" p="4">
       <Spinner />
       <Text color="gray">Fetching access credentials…</Text>
     </Flex>
